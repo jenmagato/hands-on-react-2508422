0ec87a9ea804a91dee202612eb21c2dd990bc815
jenmagato <104409944+jenmagato@users.noreply.github.com>
2022-11-23 02:48:20 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           <img src="images/group.svg" alt="StarGazers Group" />
           <Welcome name={name} /> 
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+          <ListCast  />
           {memberInfo && <Modals member={memberInfo} handleChange={(info) => { setMemberInfo(cast[info]) }} handleClose={() => { setMemberInfo(null) }} />}
           <Support /> 
         </hgroup>
--- src/components/ListCast.jsx
+++ src/components/ListCast.jsx
@@ -1,4 +1,35 @@
-export default ({ cast, onChoice }) => {
+// export default ({ cast, onChoice }) => {
+//   return (
+//     <div style={{
+//       display: "grid",
+//       gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
+//       gap: `1rem`,
+//       marginBottom: '1rem'
+//     }}>
+//       {
+//         cast.map(member => (
+//           <a onClick={() => { onChoice(member) }} key={member.id} data-tooltip={member.name}>
+//             <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
+//           </a>
+//         ))
+//       }
+//     </div>
+//   )
+// }
+
+import {useState, useEffect} from "react";
+export default() => {
+  const [cast, setCast] = useState([]);
+
+  async function fetchCast(){
+    const response = await fetch('cast.json');
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
+
   return (
     <div style={{
       display: "grid",
@@ -8,10 +39,8 @@ export default ({ cast, onChoice }) => {
     }}>
       {
         cast.map(member => (
-          <a onClick={() => { onChoice(member) }} key={member.id} data-tooltip={member.name}>
-            <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
-          </a>
-        ))
+          <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
+        ) )
       }
     </div>
   )
